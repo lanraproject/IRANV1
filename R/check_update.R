070177d52a0dfccc4581588c53eca2e3a98bccd6
lanraproject <bhilan123@gmail.com>
2026-04-19 19:27:30 +0700

    Modify version update notification logic
    Updated the message for version update notification to use 'stop' instead of 'message'.

--- R/check_update.R
+++ R/check_update.R
@@ -3,18 +3,14 @@ check_update <- function() {
     # ambil versi lokal
     local_version <- as.character(utils::packageVersion("IRANV1"))
     
-    # ambil versi dari DESCRIPTION di GitHub
+    # ambil versi dari GitHub (DESCRIPTION)
     url <- "https://raw.githubusercontent.com/lanraproject/IRANV1/main/DESCRIPTION"
     desc <- readLines(url, warn = FALSE)
     
     remote_version <- sub("Version: ", "", desc[grep("^Version:", desc)])
     
     if (length(remote_version) > 0 && local_version != remote_version) {
-      message("🚨 Versi baru tersedia!")
-      message("Versi Anda: ", local_version)
-      message("Versi terbaru: ", remote_version)
-      message("Silakan update dengan:")
-      message('remotes::install_github("lanraproject/IRANV1")')
+      stop("Versi Anda sudah usang.\nSilakan update dulu:\nremotes::install_github('lanraproject/IRANV1')")
     }
     
   }, silent = TRUE)
