4d342eafb2d6097c84a227bf144765ccacbe04ab
lanraproject <bhilan123@gmail.com>
2026-04-19 19:37:06 +0700

    Enhance check_update to fetch remote version correctly
    Refactor check_update function to improve version fetching logic.

--- R/check_update.R
+++ R/check_update.R
@@ -1,13 +1,15 @@
 check_update <- function() {
   try({
-    # ambil versi lokal
     local_version <- as.character(utils::packageVersion("IRANV1"))
-    
-    # ambil versi dari GitHub (DESCRIPTION)
+
     url <- "https://raw.githubusercontent.com/lanraproject/IRANV1/main/DESCRIPTION"
     desc <- readLines(url, warn = FALSE)
     
-    remote_version <- sub("Version: ", "", desc[grep("^Version:", desc)])
+    remote_version_line <- desc[grep("^Version:", desc)]
+
+    if (length(remote_version_line) == 0) return()
+
+remote_version <- sub("Version: ", "", remote_version_line)
     
     if (length(remote_version) > 0 && local_version != remote_version) {
       stop("Versi Anda sudah usang.\nSilakan update dulu:\nremotes::install_github('lanraproject/IRANV1')")
